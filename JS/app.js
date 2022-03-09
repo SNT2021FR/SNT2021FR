303d5a742ad91477ebd159492cc194d409ce3bd7
Félix <77358382+ftdfr@users.noreply.github.com>
2022-03-09 17:52:32 +0100

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
 let bouton = document.getElementById("top-button");
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY > -10 && window.scrollY < 500) {
+    if(window.scrollY > -10 && window.scrollY < 2500) {
         bouton.style.display="none";
     } else {
         bouton.style.display="block";
@@ -9,9 +9,7 @@ window.addEventListener('scroll', () => {
 });
 
 const loader = document.querySelector('.loader');
-const site = document.querySelector('.site');
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     loader.classList.add('fondu-out');
-    site.style.display="block"
 });
