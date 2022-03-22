7f5ca3622f86c5d15c40a20c9c9c80b89522a1ad
Félix <77358382+ftdfr@users.noreply.github.com>
2022-03-22 15:16:16 +0100

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -13,3 +13,9 @@ const loader = document.querySelector('.loader');
 window.addEventListener('load', () => {
     loader.classList.add('fondu-out');
 });
+
+const checkbox = document.getElementsByClassName('checkbox')[0];
+
+checkbox.addEventListener('click', () => {
+    alert('Merci d\' avoir lu le site !');
+});
