9ea48404fd33e6d56abb19594d4b805f696f063c
Félix <77358382+ftdfr@users.noreply.github.com>
2022-03-23 15:38:14 +0100

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,14 @@
-let bouton = document.getElementById("top-button");
+const bouton = document.getElementById("top-button");
+const loader = document.querySelector('.loader');
+const checkbox = document.getElementsByClassName('checkbox')[0];
+const TL = gsap.timeline({paused: true}); 
+const site = document.querySelector('.site');
+
+window.addEventListener('load', () => { 
+    loader.classList.add('fondu-out');
+    TL.staggerFrom(site, 0.5, {opacity: 0, ease: "power2.out"});
+    TL.play();
+});
 
 window.addEventListener('scroll', () => {
     if(window.scrollY > -10 && window.scrollY < 2500) {
@@ -8,14 +18,9 @@ window.addEventListener('scroll', () => {
     }
 });
 
-const loader = document.querySelector('.loader');
-
-window.addEventListener('load', () => {
-    loader.classList.add('fondu-out');
-});
-
-const checkbox = document.getElementsByClassName('checkbox')[0];
-
 checkbox.addEventListener('click', () => {
     alert('Merci d\'avoir lu le site !');
 });
+
+
+
